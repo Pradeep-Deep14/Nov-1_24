dbfdb5bba03eae9156c4aebc3e340d3841412dc0
Pradeep-Deep14 <123350286+Pradeep-Deep14@users.noreply.github.com>
2024-11-01 20:34:50 +0530

    Add files via upload

---
+++ Actor Director Problem.sql
@@ -0,0 +1,40 @@
+CREATE TABLE IF NOT EXISTS ActorDirector (
+    actor_id INT,
+    director_id INT,
+    timestamp INT
+);
+
+INSERT INTO ActorDirector (actor_id, director_id, timestamp) VALUES 
+    (1, 1, 0),
+    (1, 1, 1),
+    (1, 1, 2),
+    (1, 2, 3),
+    (1, 2, 4),
+    (2, 1, 5),
+    (2, 1, 6);
+
+SELECT * FROM ACTORDIRECTOR
+
+/*
+Write a SQL query for a report that provides the pairs (actor_id, director_id) 
+where the actor have co-worked with the director at least 3 times.
+
+
+Result table:
++-------------+-------------+
+| actor_id    | director_id |
++-------------+-------------+
+| 1           | 1           |
++-------------+-------------+
+
+*/
+
+SELECT ACTOR_ID,
+       DIRECTOR_ID
+	  -- COUNT(*)
+FROM ACTORDIRECTOR
+GROUP BY 1,2
+HAVING COUNT(*)>=3
+
+
+
